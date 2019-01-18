f91d971df0d69f7b25619f8b94b917b375a631e4
Sidney SISSAOUI <shideneyu@gmail.com>
2019-01-18 15:44:17 +0100

    Require: Update

--- lib/blockchair_client.rb
+++ lib/blockchair_client.rb
@@ -1,14 +1,12 @@
 require 'httparty'
 require 'active_support/inflector'
-require './lib/blockchair_client/endpoints/base' # need to be load before all blockchairclient/endpoints/*.rb
+require './lib/blockchair_client/endpoints/base' # need to be loaded before blockchairclient/endpoints/*.rb
 
 ruby_project_files = Dir[File.join(File.dirname(__FILE__), '**', '*.rb')]
 
 ruby_project_files.sort_by!{ |file_name| file_name.downcase }.each do |path|
   require_relative path
 end
-
-$stdout.sync = true
 
 module BlockchairClient
 
